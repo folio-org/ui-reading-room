882405e6991ab35b16e6b287f69c5606735d994e
Priyanka Terala <priyanka_terala@epam.com>
2024-05-27 16:50:09 +0530

    UIRR-3 - Add Reading Room Access App into App List

--- src/index.js
+++ src/index.js
@@ -1,60 +1,48 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import Switch from 'react-router-dom/Switch';
-import Route from 'react-router-dom/Route';
-import Application from './routes/application';
-import ExamplePage from './routes/example-page';
-import Settings from './settings';
+import { Switch, Route } from 'react-router-dom';
+
+import ScanPatron from './ScanPatron';
 
 /*
   STRIPES-NEW-APP
   This is the main entry point into your new app.
 */
 
-class ReadingRoom extends React.Component {
-  static propTypes = {
-    match: PropTypes.object.isRequired,
-    showSettings: PropTypes.bool,
-    stripes: PropTypes.shape({
-      connect: PropTypes.func
-    })
+const ReadingRoom = (props) => {
+  const { match: { path }, location } = props;
+
+  const NoMatch = () => {
+    return (
+      <div data-testid="noMatch">
+        <h2>Uh-oh!</h2>
+        <p>
+          How did you get to
+          {' '}
+          <tt>{location.pathname}</tt>
+          ?
+        </p>
+      </div>
+    );
   };
 
-  constructor(props) {
-    super(props);
+  return (
+    <Switch>
+      <Route
+        path="/reading-room"
+        component={ScanPatron}
+      />
+      <Route component={() => NoMatch()} />
+    </Switch>
+  );
+};
 
-    this.connectedExamplePage = props.stripes.connect(ExamplePage);
-  }
-
-  render() {
-    const {
-      showSettings,
-      match: {
-        path
-      }
-    } = this.props;
-
-    if (showSettings) {
-      return <Settings {...this.props} />;
-    }
-    return (
-      <Switch>
-        <Route
-          path={path}
-          exact
-          component={Application}
-        />
-        <Route
-          path={`${path}/examples`}
-          exact
-          component={this.connectedExamplePage}
-        />
-      </Switch>
-    );
-  }
-}
+ReadingRoom.propTypes = {
+  stripes: PropTypes.shape({
+    connect: PropTypes.func.isRequired,
+  }).isRequired,
+  location: PropTypes.object.isRequired,
+  match: PropTypes.object.isRequired,
+};
 
 export default ReadingRoom;
