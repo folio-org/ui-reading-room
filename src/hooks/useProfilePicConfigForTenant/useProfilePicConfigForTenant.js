61630c386591d3ffa17d30de95771bdfe2e2255b
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2026-03-19 16:35:19 +0200

    UIRR-46: Change users/settings/entries to users/settings. Update users.settings interface version to 2.0.

--- src/hooks/useProfilePicConfigForTenant/useProfilePicConfigForTenant.js
+++ src/hooks/useProfilePicConfigForTenant/useProfilePicConfigForTenant.js
@@ -13,7 +13,7 @@ const useProfilePicConfigForTenant = () => {
 
   const { data } = useQuery({
     queryKey: [namespace],
-    queryFn: () => ky.get('users/settings/entries').json(),
+    queryFn: () => ky.get('users/settings').json(),
   });
 
   const enabled = data?.settings?.find(setting => setting.key === PROFILE_PICTURE_CONFIG_KEY)?.value?.enabled;
